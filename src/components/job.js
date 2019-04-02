e9dad590c290af8831c4c29a93f8fb627ff57f0a
AMILKAR ESLAVA GALVIS <AMILKAR@sis.virtual.uniandes.edu.co>
2019-04-02 17:30:19 -0500

    funcional

--- src/components/job.js
+++ src/components/job.js
@@ -1,17 +1,34 @@
 import React from 'react';
+import {FormattedDate, FormattedRelative,FormattedNumber,FormattedPlural} from 'react-intl';
+
 
 export default class Job extends React.Component {
 
-  	render() {
-  		return (
-  			<tr>
-  				<th scope="row">{this.props.offer.id}</th>
-      			<td>{this.props.offer.name}</td>
-  				<td>{this.props.offer.company}</td>
-      			<td>{this.props.offer.salary}</td>
-      			<td>{this.props.offer.city}</td>
-      			<td>{this.props.offer.date}</td>
-  			</tr>
-  		);
+	render() {
+		
+		return (
+			<tr>
+				<th scope="row">{this.props.offer.id}</th>
+				<td>{this.props.offer.name}</td>
+				<td>{this.props.offer.company}</td>
+				<td><FormattedPlural
+					value={this.props.offer.salary}
+					one={this.props.offer.salary+' million'}
+					other={this.props.offer.salary+' millions'}
+				/></td>
+				<td>{this.props.offer.city}</td>
+				<td>
+					<FormattedDate
+						value={new Date(this.props.offer.date)}
+						year='numeric'
+						month='long'
+						day='numeric'
+						weekday='long'
+					/>
+					{' '} (<FormattedRelative value={new Date(this.props.offer.date)} />)
+				</td>
+				<td><FormattedNumber value={this.props.offer.visits} /></td>
+			</tr>
+		);
 	}
 }
--- src/components/jobsList.js
+++ src/components/jobsList.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import Job from "./job";
+import {FormattedMessage} from 'react-intl';
+
 
 export default class JobsList extends React.Component {
 
@@ -11,7 +13,8 @@ export default class JobsList extends React.Component {
     	  "company": "Schneider Electric", 
     	  "salary": 4.5,
     	  "city": "Bogotá, Colombia",
-        "date": "2019-03-26"
+        "date": "2019-03-26",
+        "visits": 1
       }, 
       {
         "id": "0002",
@@ -19,7 +22,8 @@ export default class JobsList extends React.Component {
         "company": "Google Inc.", 
         "salary": 20,
         "city": "Palo Alto, CA, USA",
-        "date": "2019-03-27"
+        "date": "2019-03-27",
+        "visits": 100
       },
       {
         "id": "0003",
@@ -27,7 +31,8 @@ export default class JobsList extends React.Component {
         "company": "Clínica La Aurora", 
         "salary": 1,
         "city": "Cali, Colombia",
-        "date": "2019-03-28"
+        "date": "2019-03-28",
+        "visits": 10000
       }
     ]
   };
@@ -37,13 +42,26 @@ export default class JobsList extends React.Component {
       <div>
         <table className="table">
           <thead className="thead-dark">
-            <tr>
+          <tr>
               <th scope="col">#</th>
-              <th scope="col">Position</th>
-              <th scope="col">Company</th>
-              <th scope="col">Salary</th>
-              <th scope="col">Ciudad</th>
-              <th scope="col">Publicacition date</th>
+              <th scope="col">
+                <FormattedMessage id="Position"/>
+              </th>
+              <th scope="col">
+                <FormattedMessage id="Company"/>
+              </th>
+              <th scope="col">
+                <FormattedMessage id="Salary"/>
+              </th>
+              <th scope="col">
+                <FormattedMessage id="City"/>
+              </th>
+              <th scope="col">
+                <FormattedMessage id="PulibicationDate"/>
+              </th>
+              <th scope="col">
+                <FormattedMessage id="Views"/>
+              </th>
             </tr>
           </thead>
           <tbody>
